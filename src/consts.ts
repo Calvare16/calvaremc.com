5b158bdd99c249cc847b4439b9685aee1609cda1
Zander Lewis <zander@zanderlewis.dev>
2025-10-03 19:56:08 -0400

    push

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Calvare';
+export const SITE_DESCRIPTION = 'Minecraft content creator, YouTuber, and music maker';
